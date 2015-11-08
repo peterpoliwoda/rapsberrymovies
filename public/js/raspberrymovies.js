7dbce2669bf587fdec15d238ca12e12b27bc3eec
Peter Poliwoda <peterpoliwoda@ie.ibm.com>
2015-11-08 13:36:01 +0000

    Moi and I

--- public/js/raspberrymovies.js
+++ public/js/raspberrymovies.js
@@ -26,3 +26,42 @@ ref.on("value", function(snapshot) {
 }, function (errorObject) {
   console.log("The read failed: " + errorObject.code);
 });
+
+var raspberryMovies = angular.module('raspberryMovies', ['ngRoute']);
+
+    // configure our routes
+    raspberryMovies.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/choose', {
+                templateUrl : 'pages/choose.html',
+                controller  : 'chooseController'
+            })
+
+            // route for the contact page
+            .when('/watch', {
+                templateUrl : 'pages/watch.html',
+                controller  : 'watchController'
+            });
+    });
+
+    // create the controller and inject Angular's $scope
+    raspberryMovies.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+
+    raspberryMovies.controller('chooseController', function($scope) {
+        $scope.message = 'Choose your next movie for your friend.';
+    });
+
+    raspberryMovies.controller('watchController', function($scope) {
+        $scope.message = 'Your friend chose a movie for you. Watch it here.';
+    });
